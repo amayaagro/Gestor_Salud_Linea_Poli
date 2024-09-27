38b9e2aef10c46ef4c9e05ffc0efaffc8daea523
Alexander Amaya <105259049+amayaagro@users.noreply.github.com>
2024-09-26 20:20:13 -0500

    Add files via upload

---
+++ hms/doctor/include/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> GSL</span>. <span>Todos los derechos reservados</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
