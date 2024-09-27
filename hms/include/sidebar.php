467ac58b5e9b84a438e0ba5c2ab2f654f9330da2
Alexander Amaya <105259049+amayaagro@users.noreply.github.com>
2024-09-26 19:47:38 -0500

    Add files via upload

---
+++ hms/include/sidebar.php
@@ -0,0 +1,66 @@
+<!-- Barra Lateral -->
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>						
+						<!-- Inicio del Manú de Navegación -->
+						<div class="navbar-title">
+							<span>Menú de Navegación</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Inicio </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="book-appointment.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-pencil-alt"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Agendar Cita </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+							<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Historial de Citas </span>
+										</div>
+									</div>
+								</a>
+							</li>
+<li>
+								<a href="manage-medhistory.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Historia Clínica </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+						</ul>
+						<!-- Fin del Manú de Navegación -->
+						
+					</nav>
+					</div>
+			</div>
